5d882827a9e7d1e32fe186896b4ac535ca0ef2d1
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-08-27 04:05:36 +0600

    recap for loop

--- for_loop/for_loop.c
+++ for_loop/for_loop.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
    int n;
    printf("take a number: ");
-   scanf("%d",&n);
-   printf("%d\n",n);
-   return 0;  
+   scanf("%d", &n);
+   printf("%d\n", n);
+
+   for (int i = 1; i <= n; i++)
+   {
+      printf("%d. Sakil you are a brilliant boy,but tumi je olosota koro ayta kintu thik na,tomake jibon theke olosotake bye bola uchit\n", i);
+   }
+   return 0;
 }
