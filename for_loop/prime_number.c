a6c85c99c2961132454a4946af031ce51f30fb05
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-08-28 09:16:13 +0600

    prime number

---
+++ for_loop/prime_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int is_prime=1;
+    if (n < 2)
+    {
+        printf("%d - not a prime number", n);
+        return 0;
+    }
+    else            
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if(n%i==0){
+                is_prime=0;
+            }
+        }
+    }
+    if(!is_prime){
+        printf("%d - not a prime number",n);
+    }
+    if(is_prime){
+        printf("%d - prime number",n);
+    }
+    return 0;
+}
