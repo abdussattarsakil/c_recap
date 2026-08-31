73f3379e35be1e226c497a246dd95894401e6913
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-09-01 01:04:50 +0600

    drawing problem stuck......

---
+++ B_Drawing_X.c
@@ -0,0 +1,12 @@
+//cf link: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/B
+//He wanted to do so by grouping snippets of slashes /, backslashes \, asterisks * and a capital X letter in an N×N square as shown in the sample.
+
+// input 5
+
+//1           \***/     4-s=3   
+//2           *\*/*     4-s=3     
+//3           **X**     5-s=4      
+//4           */*\*     4-s=3      
+//5           /***\     5-s=3      
+
+
