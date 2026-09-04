22a694d3a02a32e04cc68f0fdd65e7b9d68517cb
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-09-05 00:54:09 +0600

    use algorithm to solve this problem

--- Z_Hard_Compare.c
+++ Z_Hard_Compare.c
@@ -1,54 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-   int a, b, c, d;
-   scanf("%d %d %d %d", &a, &b, &c, &d);
-   long long int ab = a;
-   long long int cd = c;
-   if (a == c && b == d)
-   {
-      printf("NO");
-      return 0;
-   }
-   if (b == d)
-   {
-      if (a > c)
-      {
-         printf("YES");
-      }
-      if (a < c)
-      {
-         printf("NO");
-      }
-      return 0;
-   }
-   if (a > c && b > d)
-   {
-      printf("YES");
-      return 0;
-   }
-   if (a < c && b < d)
-   {
-      printf("NO");
-      return 0;
-   }
-   for (int i = 1; i < b; i++)
-   {
-      ab *= a;
-      printf("%d\n",ab);
-   }
-   for (int i = 1; i < d; i++)
-   {
-      cd *= c;
-   }
-   if (ab > cd)
-   {
-      printf("YES");
-   }
-   else
-   {
-      printf("NO");
-   }
-   printf("%lld\n%lld",ab,cd);
-   return 0;
+    long long a, b, c, d;
+
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    double left = b * log(a);
+    double right = d * log(c);
+
+    if (left > right)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+    return 0;
 }
