ef2e1717e17a422d8ecf4df2d5e6dbed8d1fa356
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-09-07 20:14:45 +0600

    learn gets() but this doesn't work because modern gcc c11

--- string/string.c
+++ string/string.c
@@ -7,16 +7,27 @@
 //    return 0;
 // }
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main()
-{
-   char str1[20] = "Bangladesh";                    // অটোমেটিক '\0' যুক্ত
-   char str2[20] = {'D', 'h', 'a', 'k', 'a', '\0'}; // ম্যানুয়ালি '\0' যুক্ত
+// int main()
+// {
+//    char str1[20] = "Bangladesh";                    // অটোমেটিক '\0' যুক্ত
+//    char str2[20] = {'D', 'h', 'a', 'k', 'a', '\0'}; // ম্যানুয়ালি '\0' যুক্ত
 
-   printf("str1: %s\n", str1); //Bangladesh
-   printf("str2: %s\n", str2);//Dhaka
+//    printf("str1: %s\n", str1); //Bangladesh
+//    printf("str2: %s\n", str2);//Dhaka
 
-   return 0;
-}
+//    return 0;
+// }
+
+//space soho string print
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//    char str[20];
+//    gets(str);
+//    printf("%s",str);
+//    return 0;  
+//}//gets() function-টা C11 standard থেকে remove করা হয়েছে, কারণ এটা unsafe। Buffer overflow হতে পারে।
