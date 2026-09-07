683d89f211712f270ea06e97906be721dfaef3dd
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-09-07 15:51:07 +0600

    string convert a number -problem

--- O_Calculator.c
+++ O_Calculator.c
@@ -1,6 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-   
-   return 0;  
+    char input[100000];
+    scanf("%s", &input);
+    // printf("%s", input);
+    printf("%c\n", input[0]);
+    int length = strlen(input);
+    // printf("%d",length);
+    int s;
+    for (int i = 0; i < length; i++)
+    {
+        if (input[i] == '+')
+        {
+            s = i;
+            break;
+        }
+    }
+    printf("%d\n", s);
+    int a = 0;
+    for (int i = 0; i < s; i++)
+    {
+        a *= 10;
+        a += input[i];
+    }
+    int b = 0;
+    for (int i = s + 1; i < length; i++)
+    {
+        b *= 10;
+        b += input[i];
+        printf("%d\n",b-48);
+    }
+    // printf("%d", b-48);
+    return 0;
 }
